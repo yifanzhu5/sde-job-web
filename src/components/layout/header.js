b6297a93f47be528d766969aa671bbcd626130fb
y575zhu <y575zhu@uwaterloo.ca>
2022-02-22 18:34:51 -0500

    keep header on top of the page

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -12,49 +12,12 @@ import { UserOutlined } from '@ant-design/icons';
 
 const {SubMenu} = Menu;
 
-// const state = {
-//     current: 'mail',
-//   };
-
-// const handleClick = (e) => {
-//     console.log('click ', e);
-//     this.setState({ current: e.key });
-//   };
-
-// class CustomHeader extends React.Component {
-//    render() {
-//        return (
-//            <Menu mode="horizontal">
-//                <Menu.Item key="mail" icon={<MailOutlined />}>
-//                    Mail us
-//                </Menu.Item>
-//                <Menu.Item key="alipay">
-//                    Change Routes
-//                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer" />
-//                </Menu.Item>
-//            </Menu>
-//
-//        );
-//    }
-// }
-
-// <div className={"job-card-head"}>
-//     <div className={"job-card-head-link"}>
-//         <div className={"job-card-head-link-title"}>{this.props.title}
-//         </div>
-//         <Tooltip title="Apply">
-//             <a target="_blank" href={this.props.apply_url}>
-//                 <LinkOutlined/>
-//             </a>
-//         </Tooltip>
-//     </div>
-
 class CustomHeader extends React.Component {
     render() {
         return (
             <div class={"custom-header"}>
                 <div class={"custom-header-left"}>
-                    CANADA SDE CAREERS
+                    CASDEJOBS
                 </div>
                 <div class={"custom-header-mid"}>
                     <Menu mode="horizontal">
