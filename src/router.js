a252181551f1f933646705e8102f489b0cb9a94a
c332huan <c332huan@uwaterloo.ca>
2022-02-23 15:38:59 -0500

    update left-col scss; scrollToTop

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Switch, HashRouter, Route,  useLocation} from "react-router-dom";
+import { BrowserRouter, Switch, Route, useLocation } from "react-router-dom";
 import Home from "./page/App/home";
 import PageError from "./page/App/404";
 
@@ -21,6 +21,22 @@ const routes = [
     },
 
 ];
+
+class ScrollToTop extends React.Component {
+    componentWillReceiveProps(nextProps) {
+        // 当路由切换时
+        if (this.props.location !== nextProps.location) {
+            const layoutContentNode = document.querySelector('.layout-content')
+            // 重点在这里， 需要是在 产生 scroll 的父元素 上进行 scrollTo，而不能在window上，因为在项目中，window不是产生滑动的父元素
+            layoutContentNode && layoutContentNode.scrollTo(0, 0)
+        }
+    }
+
+    render() {
+        return this.props.children
+    }
+}
+
 class Router extends React.Component {
     render() {
         // return (
@@ -42,15 +58,18 @@ class Router extends React.Component {
         // );
         return (
             <BrowserRouter>
-                <CustomHeader></ CustomHeader>
-                <Switch>
-                    {routes.map((route)=>(
-                        <Route path={route.path}>
-                            {route.component}
-                        </Route>
-                    ))}
-                </Switch>
-                <CustomFooter></CustomFooter>
+                <ScrollToTop>
+                    <CustomHeader></ CustomHeader>
+                    <Switch>
+
+                        {routes.map((route) => (
+                            <Route path={route.path}>
+                                {route.component}
+                            </Route>
+                        ))}
+                    </Switch>
+                    <CustomFooter></CustomFooter>
+                </ScrollToTop>
             </BrowserRouter>
         );
 
