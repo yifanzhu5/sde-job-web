fb03cc4d61933d142cbc95be1f79f4f84e0876e6
y575zhu <y575zhu@uwaterloo.ca>
2022-03-28 10:32:03 -0400

    add favJobs

--- src/components/job-show/jobCard.js
+++ src/components/job-show/jobCard.js
@@ -1,68 +1,100 @@
 import React from 'react';
 
 
-import { Card, Tooltip, Space } from 'antd';
+import {Card, Tooltip, Space, message} from 'antd';
 import {
-  LinkOutlined,
-  BankOutlined,
-  FieldTimeOutlined,
-  EnvironmentOutlined
+    LinkOutlined,
+    BankOutlined,
+    FieldTimeOutlined,
+    EnvironmentOutlined,
+    HeartTwoTone,
+    getTwoToneColor,
+    setTwoToneColor
 } from '@ant-design/icons';
+import axios from "axios";
 
 class JobCard extends React.Component {
-  // console.log(props.jobTitle)
-  // props.applyUrl
-  //
-  render() {
-    return (
-      <a class="job-card" target="_blank" href={this.props.from_url}>
-        <Card
-          hoverable="true"
-        >
-          <div class={"job-card-head"}>
-            <div class={"job-card-head-link"}>
-              <div class={"job-card-head-link-title"}>{this.props.title}
-              </div>
-              <Tooltip title="Apply">
-                <a target="_blank" href={this.props.apply_url}>
-                  <LinkOutlined />
-                </a>
-              </Tooltip>
-            </div>
+    handleLike = () => {
+        const config = {
+            headers: {
+                token: localStorage.getItem('token')
+            }
+        }
+        console.log('currentColor: ', getTwoToneColor())
+        let isAdd = true
+        if(getTwoToneColor() == '#F00') {
+            isAdd = false
+        }
+        const params = {
+            "id": this.props.id,
+            "isAdd": isAdd
+        }
+        axios.post(`/api/v1/updateFavJobs`, params, config).then((res) => {
+            if(isAdd) {
+                setTwoToneColor('#F00')
+            } else {
+                setTwoToneColor('#1890ff')
+            }
+        }).catch((data) => {
+            console.log('error', data)
+        })
+    }
 
-            <div class="job-card-head-info">
-              <div class={"job-card-head-info-layer1"}>
-                <Space size="small">
-                  <Tooltip title="company">
-                    <div>
-                      <BankOutlined />
-                      {this.props.company}
+    render() {
+        return (
+                <Card hoverable="true">
+                    <div class={"job-card-head"}>
+                        <div class={"job-card-head-link"}>
+                            <a className="job-card" target="_blank"  href={this.props.from_url}>
+                                <div className={"job-card-head-link-title"}>{this.props.title}
+                                </div>
+                            </a>
+                          <div class={"job-card-head-link-operation"}>
+                                  <Tooltip title="like">
+                                          <HeartTwoTone onClick={this.handleLike} />
+                                  </Tooltip>
+                                  <Tooltip title="Apply">
+                                      <a target="_blank" href={this.props.apply_url}>
+                                          <LinkOutlined/>
+                                      </a>
+                                  </Tooltip>
+                          </div>
+                        </div>
+
+                        <div class="job-card-head-info">
+                            <div class={"job-card-head-info-layer1"}>
+                                <Space size="small">
+                                    <Tooltip title="company">
+                                        <div>
+                                            <BankOutlined/>
+                                            {this.props.company}
+                                        </div>
+                                    </Tooltip>
+                                    <Tooltip title="Cities">
+                                        <div>
+                                            <EnvironmentOutlined/>
+                                            {this.props.cities}
+                                        </div>
+                                    </Tooltip>
+                                </Space>
+                            </div>
+                            {(this.props.publish_time != "") &&
+                                <Tooltip title="Publish time">
+                                    <div>
+                                        <FieldTimeOutlined/>
+                                        {new Date(this.props.publish_time * 1000).toLocaleDateString()}
+                                    </div>
+                                </Tooltip>}
+                        </div>
                     </div>
-                  </Tooltip>
-                  <Tooltip title="Cities">
-                    <div>
-                      <EnvironmentOutlined />
-                      {this.props.cities}
-                    </div>
-                  </Tooltip>
-                </Space>
-              </div>
-              {(this.props.publish_time != "") &&
-                <Tooltip title="Publish time">
-                  <div>
-                    <FieldTimeOutlined />
-                    {new Date(this.props.publish_time * 1000).toLocaleDateString()}
-                  </div>
-                </Tooltip>}
-            </div>
-          </div>
-          <div class="job-card-content">
-            <div dangerouslySetInnerHTML={{ __html: this.props.content }}></div>
-          </div>
-        </Card>
-      </a>
-    )
-  }
+                    <a className="job-card" target="_blank" href={this.props.from_url}>
+                        <div className="job-card-content">
+                            <div dangerouslySetInnerHTML={{__html: this.props.content}}></div>
+                        </div>
+                    </a>
+                </Card>
+        )
+    }
 
 };
 
--- src/page/App/jobs/index.js
+++ src/page/App/jobs/index.js
@@ -1,218 +1,219 @@
 import React from "react";
 import Qs from 'qs'
-import { Form, Input, Button, Checkbox, Divider, Pagination, Space } from 'antd';
+import {Form, Input, Button, Checkbox, Divider, Pagination, Space} from 'antd';
 
 import axios from 'axios'
 // import {request} from '../../../utils/request'
 
 import JobCard from "../../../components/job-show/jobCard";
 
-const { Search } = Input;
+const {Search} = Input;
 
 const companyOptions = [
-  { label: 'Google', value: 'Google' },
-  { label: 'Amazon', value: 'Amazon' },
-  { label: 'Shopify', value: 'Shopify' },
-  { label: 'Microsoft', value: 'Microsoft' },
-  { label: 'Others', value: 'others' }
+    {label: 'Google', value: 'Google'},
+    {label: 'Amazon', value: 'Amazon'},
+    {label: 'Shopify', value: 'Shopify'},
+    {label: 'Microsoft', value: 'Microsoft'},
+    {label: 'Others', value: 'others'}
 ];
 
 const cityOptions = [
-  { label: 'Remote', value: 'Remote' },
-  { label: 'Vancouver', value: 'Vancouver' },
-  { label: 'Toronto', value: 'Toronto' },
-  { label: 'Montreal', value: 'Montreal' },
-  { label: 'Ottawa', value: 'Ottawa' },
-  { label: 'Waterloo', value: 'Waterloo' },
-  { label: 'Others', value: 'others' }
+    {label: 'Remote', value: 'Remote'},
+    {label: 'Vancouver', value: 'Vancouver'},
+    {label: 'Toronto', value: 'Toronto'},
+    {label: 'Montreal', value: 'Montreal'},
+    {label: 'Ottawa', value: 'Ottawa'},
+    {label: 'Waterloo', value: 'Waterloo'},
+    {label: 'Others', value: 'others'}
 ];
 
 class Jobs extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
-
-  state = {
-    page_size: 20,
-    current_page: 1,
-    count: 0,
-    keywords: null,
-    cities: [],
-    companys: [],
-    has_remote: null,
-    jobs: [],
-  };
-
-  getRouteData = () => {
-    const query = new URLSearchParams(this.props.location.search)
-    this.setState({
-      page_size: query.get('page_size') || 20,
-      current_page: query.get('current_page') || 1,
-      keywords: query.get('keywords') || null,
-      cities: query.get('cities') || [],
-      companys: query.get('companys') || [],
-      has_remote: query.get('has_remote') || null,
-    }, () => {
-      this.getData()
+    constructor(props) {
+        super(props);
     }
-    )
-  }
 
-  getData = () => {
-    // Locations array have Remote.
-    // submitted location should not inclucde Remote
-    let city_submit = [];
-    Object.assign(city_submit, this.state.cities);
-    if (city_submit.includes('Remote')) {
-      this.setState({ has_remote: true })
-      let index_i = city_submit.indexOf('Remote')
-      city_submit.splice(index_i, 1)
-    } else {
-      this.setState({ has_remote: null })
+    state = {
+        page_size: 20,
+        current_page: 1,
+        count: 0,
+        keywords: null,
+        cities: [],
+        companys: [],
+        has_remote: null,
+        jobs: [],
+    };
+
+    getRouteData = () => {
+        const query = new URLSearchParams(this.props.location.search)
+        this.setState({
+                page_size: query.get('page_size') || 20,
+                current_page: query.get('current_page') || 1,
+                keywords: query.get('keywords') || null,
+                cities: query.get('cities') || [],
+                companys: query.get('companys') || [],
+                has_remote: query.get('has_remote') || null,
+            }, () => {
+                this.getData()
+            }
+        )
     }
-    const params = {
-      "keywords": this.state.keywords,
-      "cities": city_submit,
-      "companys": this.state.companys,
-      "page_size": this.state.page_size,
-      "current_page": this.state.current_page,
-      "has_remote": this.state.has_remote
-    }
-    const search_str = Qs.stringify(params, { skipNulls: true })
-    let ppath = {
-      pathname: '/jobs',
-      search: search_str
-    }
-    this.props.history.push(ppath)
-    axios.post('api/v1/jobs/search', params).then((res) => {
-      this.setState({
-        'jobs': res.data.jobs,
-        'count': res.data.count,
-      })
-      document.documentElement.scrollTo(0,0)
-    }).catch((data) => {
-      console.log('error', data)
-    })
-  };
+
+    getData = () => {
+        // Locations array have Remote.
+        // submitted location should not inclucde Remote
+        let city_submit = [];
+        Object.assign(city_submit, this.state.cities);
+        if (city_submit.includes('Remote')) {
+            this.setState({has_remote: true})
+            let index_i = city_submit.indexOf('Remote')
+            city_submit.splice(index_i, 1)
+        } else {
+            this.setState({has_remote: null})
+        }
+        const params = {
+            "keywords": this.state.keywords,
+            "cities": city_submit,
+            "companys": this.state.companys,
+            "page_size": this.state.page_size,
+            "current_page": this.state.current_page,
+            "has_remote": this.state.has_remote
+        }
+        const search_str = Qs.stringify(params, {skipNulls: true})
+        let ppath = {
+            pathname: '/jobs',
+            search: search_str
+        }
+        this.props.history.push(ppath)
+        axios.post('api/v1/jobs/search', params).then((res) => {
+            this.setState({
+                'jobs': res.data.jobs,
+                'count': res.data.count,
+            })
+            document.documentElement.scrollTo(0, 0)
+        }).catch((data) => {
+            console.log('error', data)
+        })
+    };
 
 
-  onJobChange = (pg) => {
-    this.setState({ current_page: pg }, () => {
-      this.getData()
-    })
-  };
+    onJobChange = (pg) => {
+        this.setState({current_page: pg}, () => {
+            this.getData()
+        })
+    };
 
-  formFilter() {
-    return <Form
-      layout="vertical"
-      onFinish={this.getData}
-    >
-      <Form.Item
-        label="City"
-        name="City"
-      >
-        <Checkbox.Group
-          options={cityOptions}
-          onChange={(e) => {
-            this.setState({ cities: e, current_page: 1 })
-          }}
-        />
-      </Form.Item>
-      <Divider />
-      <Form.Item
-        label="Company"
-        name="companys"
-      >
-        <Checkbox.Group
-          options={companyOptions}
-          onChange={(e) => {
-            this.setState({ companys: e, current_page: 1 })
-          }}
-        />
-      </Form.Item>
-      <Form.Item >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  };
+    formFilter() {
+        return <Form
+            layout="vertical"
+            onFinish={this.getData}
+        >
+            <Form.Item
+                label="City"
+                name="City"
+            >
+                <Checkbox.Group
+                    options={cityOptions}
+                    onChange={(e) => {
+                        this.setState({cities: e, current_page: 1})
+                    }}
+                />
+            </Form.Item>
+            <Divider/>
+            <Form.Item
+                label="Company"
+                name="companys"
+            >
+                <Checkbox.Group
+                    options={companyOptions}
+                    onChange={(e) => {
+                        this.setState({companys: e, current_page: 1})
+                    }}
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    };
 
-  jobCards(page_size) {
-    return <div class="job-cards"><Space size={"large"} direction={"vertical"}>
-      {this.state.jobs.map((item) => {
-        // locations cities
-        return <JobCard
-          title={item.title}
-          company={item.company}
-          cities={item.city}
-          from_url={item.from_url}
-          apply_url={item.apply_url}
-          publish_time={item.publish_time}
-          content={item.description}
-        />
-      })
-      }</Space>
-      <Pagination
-        current={this.state.current_page}
-        total={this.state.count}
-        pageSize={this.state.page_size}
-        pageSizeOptions={["20", "50", "100"]}
-        onChange={this.onJobChange}
-        onShowSizeChange={(curr, size) => {
-          this.setState({
-            page_size: size
-          })
-        }}
-      />
-    </div>
-  }
-
-  // componentWillReceiveProps(nextProps){
-    
-  //   if(nextProps.location.search!=this.props.location.search){
-  //     console.log('will', nextProps)
-  //   }
-  // }
-
-  componentDidMount() {
-    this.getRouteData()
-  }
-
-  render() {
-    return (
-      <div class="custom-body">
-        <div class="left-col">
-          <div class="left-col-content">
-            <Search
-              placeholder="Search Keywords"
-              value={this.state.keywords}
-              allowClear
-              enterButton="Search"
-              size="large"
-              onChange={(e) => {
-                this.setState({ keywords: e.target.value });
-              }}
-              onSearch={(e) => {
-                this.setState({ keywords: e, current_page: 1 }, () => {
-                  this.getData()
-                })
-              }}
+    jobCards(page_size) {
+        return <div class="job-cards"><Space size={"large"} direction={"vertical"}>
+            {this.state.jobs.map((item) => {
+                // locations cities
+                return <JobCard
+                    id={item.id}
+                    title={item.title}
+                    company={item.company}
+                    cities={item.city}
+                    from_url={item.from_url}
+                    apply_url={item.apply_url}
+                    publish_time={item.publish_time}
+                    content={item.description}
+                />
+            })
+            }</Space>
+            <Pagination
+                current={this.state.current_page}
+                total={this.state.count}
+                pageSize={this.state.page_size}
+                pageSizeOptions={["20", "50", "100"]}
+                onChange={this.onJobChange}
+                onShowSizeChange={(curr, size) => {
+                    this.setState({
+                        page_size: size
+                    })
+                }}
             />
-            <Divider />
-            {this.formFilter()}
-          </div>
         </div>
-        <div class="right-col">
-          <div class="right-col-content">
-            <div class="count">{this.state.count || 'No'} jobs matched</div>
-            {this.jobCards(this.state.page_size)}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    }
+
+    // componentWillReceiveProps(nextProps){
+
+    //   if(nextProps.location.search!=this.props.location.search){
+    //     console.log('will', nextProps)
+    //   }
+    // }
+
+    componentDidMount() {
+        this.getRouteData()
+    }
+
+    render() {
+        return (
+            <div class="custom-body">
+                <div class="left-col">
+                    <div class="left-col-content">
+                        <Search
+                            placeholder="Search Keywords"
+                            value={this.state.keywords}
+                            allowClear
+                            enterButton="Search"
+                            size="large"
+                            onChange={(e) => {
+                                this.setState({keywords: e.target.value});
+                            }}
+                            onSearch={(e) => {
+                                this.setState({keywords: e, current_page: 1}, () => {
+                                    this.getData()
+                                })
+                            }}
+                        />
+                        <Divider/>
+                        {this.formFilter()}
+                    </div>
+                </div>
+                <div class="right-col">
+                    <div class="right-col-content">
+                        <div class="count">{this.state.count || 'No'} jobs matched</div>
+                        {this.jobCards(this.state.page_size)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Jobs;
